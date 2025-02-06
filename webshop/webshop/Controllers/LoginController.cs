a37a7b716091b1a5effa6b2c4b2f67d9ee5c30e5
Bálint Varga <vargabalint051@gmail.com>
2025-02-06 10:20:40 +0100

    Created LoginController

---
+++ webshop/webshop/Controllers/LoginController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using webshop.DTOs;
+using webshop.Models;
+
+namespace webshop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost("GetSalt/{userName}")]
+        public async Task<IActionResult> GetSalt(string username)
+        {
+            using (var context = new WebshopContext())
+            {
+                try
+                {
+                    Felhasznalok response = await context.Felhasznaloks.FirstOrDefaultAsync(x => x.LoginName == username);
+                    if (response == null)
+                    {
+                        return NotFound("Nem található felhasználó ezzel a felhasználónévvel.");
+                    }
+                    else
+                    {
+                        return Ok(response.Salt);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+
+        public async Task<IActionResult> Login(LoginDTO loginDTO)
+        {
+            using (var context = new WebshopContext())
+            {
+                try
+                {
+                    string Hash = Manager.CreateSHA256(loginDTO.TmpHash);
+                    Felhasznalok loggedUser = await context.Felhasznaloks.FirstOrDefaultAsync(u => u.LoginName == loginDTO.LoginName && u.Hash == Hash);
+                    if (loggedUser != null)
+                    {
+                        string token = Guid.NewGuid().ToString();
+                        lock (Manager.LoggedInUsers)
+                        {
+                            Manager.LoggedInUsers.Add(token, loggedUser);
+                        }
+                        return Ok(new LoggedUser
+                        {
+                            LoginName = loginDTO.LoginName,
+                            Email = loggedUser.Email
+                        });
+                    }
+                    else
+                    {
+                        return NotFound("Nem található felhasználó ezzel a felhasználónévvel.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+    }
+}
--- webshop/webshop/obj/Release/net8.0/webshop.AssemblyInfo.cs
+++ webshop/webshop/obj/Release/net8.0/webshop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webshop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+142337b4d8e01d088def11b9bcbc266757df8956")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+455ac0608d517c0d0bd304fb531f824b27c7eecf")]
 [assembly: System.Reflection.AssemblyProductAttribute("webshop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webshop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
